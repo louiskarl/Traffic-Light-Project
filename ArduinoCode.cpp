91a61a243255e0410c874eac5f5a9da23dd8de29
nbell990 <60492033+nbell990@users.noreply.github.com>
2022-10-12 14:22:56 -0400

    Update ArduinoCode.cpp
    Added comments and fixed last section which seemed to have gotten clipped from the original upload.

--- ArduinoCode.cpp
+++ ArduinoCode.cpp
@@ -27,18 +27,45 @@ void loop() {
   buttonState = digitalRead(7);
   if (buttonState == HIGH){
     pgmControl = 1;
-    while (pgmControl == 1){
+    while (pgmControl == 1){    //Locks program in loop while pgmControl flag is equal to 1. 
+                                //pgmControl flag is not set low by program, only way to exit loop is to disconnect power.
     
-    digitalWrite(8, LOW);
+    digitalWrite(8, LOW);       //Activates red light, deactivates yellow and green lights.
     digitalWrite(9, HIGH);
     digitalWrite(10, HIGH);
     
     
-    delay(17000);
-    digitalWrite(11, HIGH);
+    delay(17000);               //Waits for 17 seconds, then turns on buzzer for three seconds, then deactivates buzzer.
+    digitalWrite(11, HIGH);
     delay(3000);
     digitalWrite(11, LOW);
 
-   digitalWrite(8, HIGH);
+    digitalWrite(8, HIGH);      //Turns on green light, turns off red and yellow lights.
     digitalWrite(9, HIGH);
     digitalWrite(10, LOW);
+      
+    delay(17000);               //Waits for 17 seconds, then turns on buzzer for three seconds, then deactivates buzzer.
+    digitalWrite(11, HIGH);
+    delay(3000);
+    digitalWrite(11, LOW);
+    
+    digitalWrite(8, HIGH);      //Turns on yellow light, turns off red and green lights.
+    digitalWrite(9, LOW);
+    digitalWrite(10, HIGH);
+    
+    
+    digitalWrite(11, HIGH);      //Turns on buzzer for 3 seconds.
+    delay(3000);
+    digitalWrite(11, LOW);
+    }
+  }
+   else{
+    digitalWrite(8, LOW);       //Flashes the red light on and off once per second until the button is pushed (buttonState == LOW).
+    digitalWrite(9, HIGH);
+    digitalWrite(10, HIGH);
+    delay(1000);
+    digitalWrite(8, HIGH);
+    delay(1000);
+    
+   }
+}
